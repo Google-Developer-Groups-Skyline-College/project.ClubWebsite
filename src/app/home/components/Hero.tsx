5dbcc18d5935b3cd7a5159bb54588e69aa43135b
devalto <51037255+dev-alto@users.noreply.github.com>
2025-03-12 03:41:47 -0700

    feat: re-updated organization name

--- src/app/home/components/Hero.tsx
+++ src/app/home/components/Hero.tsx
@@ -2,12 +2,12 @@
 
 import React from 'react'
 import { ReactTyped } from 'react-typed'
-import { Element } from 'react-scroll'
+// import { Element } from 'react-scroll'
 
 import Image from 'next/image'
 
 import { IoMdArrowRoundDown } from 'react-icons/io'
-import { Link } from 'react-scroll'
+// import { Link } from 'react-scroll'
 
 import { useRef } from 'react'
 import { motion, useInView } from 'framer-motion'
@@ -31,7 +31,7 @@ const TYPED_KEYWORDS = [
     'Game Design',
     'Collaborate',
     'Delegate',
-    '$ git commit',
+    '$ git commit'
 ]
 
 export function Hero(): React.ReactNode {
@@ -40,7 +40,7 @@ export function Hero(): React.ReactNode {
 
     return (
         <div className='w-full h-screen bg-black'>
-            <Element name='hero' />
+            {/* <Element name='hero' /> */}
 
             <div className='absolute w-full h-full pointer-events-none overflow-hidden'>
                 <motion.div
@@ -75,7 +75,7 @@ export function Hero(): React.ReactNode {
                 </motion.div>
             </div>
 
-            <div className='absolute w-full h-full mx-auto flex flex-col items-center justify-center pb-24'>
+            <div className='absolute w-full h-full mx-auto flex flex-col items-center justify-center pb-24 gap-y-2'>
                 {/* mascot */}
                 <Image
                     width={80}
@@ -87,11 +87,12 @@ export function Hero(): React.ReactNode {
                 />
 
                 {/* The main texts */}
-                <h1 className='z-20 title-main text-center text-[4.6vw] xl:text-[52px] text-shadow shadow-black font-bold text-white mx-4 my-[2px] sm:my-[-8px] whitespace-pre-line leading-tight'>
-                    {'Google Developer Groups \non Campus Skyline College'}
+                <h1 className='z-20 title-main text-center text-shadow shadow-black font-bold text-white leading-tight'>
+                    <div className='text-[4.4vw] xl:text-[52px]'>Google Developer Groups on Campus </div>
+                    <div className='text-[3.2vw] xl:text-[36px] text-red-200'>The Skyline College Chapter</div>
                 </h1>
 
-                <div className='z-20 title-main w-full text-white text-center text-shadow shadow-black text-[3.25vw] sm:text-xl mt-5'>
+                <div className='z-20 title-main w-full text-white text-center text-shadow shadow-black text-[3.25vw] sm:text-xl'>
                     <p className='group'>Elevate Your Code — the <span className='group-hover:text-red-300 group-hover:font-bold transition delay-100'>Sky&apos;s</span> <span className='group-hover:text-red-300 group-hover:font-bold transition delay-300'>the</span> <span className='group-hover:text-red-300 group-hover:font-bold transition delay-500'>Limit.</span></p>
                     <h1 className='title-main font-bold text-2xl sm:pt-[4px]'>
                         {'>'} We{' '}
@@ -108,18 +109,18 @@ export function Hero(): React.ReactNode {
                 </div>
 
                 {/* Currently requires a weird arbit. offset of -180 for this particular scroll */}
-                <Link
-                    to='about'
-                    smooth={true}
-                    offset={-140}
-                    duration={1500}
+                <div
+                    // to='about'
+                    // smooth={true}
+                    // offset={-140}
+                    // duration={1500}
                     className='z-20 mt-3 relative group w-[210px] hover:w-[240px] active:scale-95 duration-150 hover:border-green-900 border-white rounded-xl hover:outline hover:outline-[4px] active:outline-4 text-white text-lg title-main drop-shadow-[0_16px_20px_rgba(0,0,0,0.7)] overflow-hidden'
                 >
                     <IoMdArrowRoundDown className='absolute w-full h-full group-hover:translate-y-0 -translate-y-[100%] duration-300 bg-gradient-to-t from-[#16a34aec] to-[#ffffff91] pointer-events-none' />
                     <button className='flex w-full h-full justify-center py-[6px] font-semibold text-center text-shadow-lg shadow-black hover:bg-green-600 bg-neutral-900 bg-opacity-50 duration-300 hover:border-green-300 overflow-hidden'>
                         <p>I&apos;m Interested</p>
                     </button>
-                </Link> 
+                </div> 
             </div>
 
             <div className='absolute w-full h-full'>
