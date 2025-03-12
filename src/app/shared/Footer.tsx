d04ea43cfd299a82903f4b2ec9baa3249e88b494
devalto <51037255+dev-alto@users.noreply.github.com>
2025-03-12 03:47:21 -0700

    feat: abstracted social buttons to own `Socials` component

--- src/app/shared/Footer.tsx
+++ src/app/shared/Footer.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-
-import { SiDiscord, SiYoutube, SiLinkedin, SiInstagram, SiGithub, SiLinktree, SiSlideshare } from 'react-icons/si'
+import Socials from './Socials'
 
 export function Footer() {
     return (
@@ -11,64 +10,7 @@ export function Footer() {
             <p className='title-main font-semibold text-xl'>Let&apos;s connect.</p>
             <p className='title-main text-neutral-500 font-semibold'>Expand your professional network with us.</p>
 
-            <div className='flex flex-row flex-wrap gap-3 justify-center py-4'>
-                <Link
-                    href='https://discord.com/invite/z5P9kccwRh'
-                    target='_blank'
-                >
-                    <div className='flex group w-[120px] sm:w-[134px] sm:hover:w-[140px] justify-center bg-neutral-700 dark:bg-neutral-900 bg-opacity-80 rounded-md gap-2 py-2 2 active:scale-95 hover:bg-blue-800 hover:outline-4 hover:text-white text-lg font-semibold active:outline-2 hover:outline-double outline-white border-[1px] border-neutral-800 hover:outline-blue-600 active:outline-neutral-200 duration-200 active:duration-200'>
-                        <SiDiscord className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
-                        Discord
-                    </div>
-                </Link>
-
-                <Link
-                    href='https://www.youtube.com/@skylinegdg'
-                    target='_blank'
-                >
-                    <div className='flex group w-[120px] sm:w-[134px] sm:hover:w-[140px] justify-center bg-neutral-700 dark:bg-neutral-900 bg-opacity-80 rounded-md gap-2 py-2 2 active:scale-95 hover:bg-red-900 hover:outline-4 hover:text-white text-lg font-semibold active:outline-2 hover:outline-double outline-white border-[1px] border-neutral-800 hover:outline-red-700 active:outline-neutral-200 duration-200 active:duration-200'>
-                        <SiYoutube className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
-                        YouTube
-                    </div>
-                </Link>
-
-                <Link
-                    href='https://www.linkedin.com/company/computer-science-club-skyline/'
-                    target='_blank'
-                >
-                    <div className='flex group w-[120px] sm:w-[134px] sm:hover:w-[140px] justify-center bg-neutral-700 dark:bg-neutral-900 bg-opacity-80 rounded-md gap-2 py-2 2 active:scale-95 hover:bg-blue-600 hover:outline-4 hover:text-white text-lg font-semibold active:outline-2 hover:outline-double outline-white border-[1px] border-neutral-800 hover:outline-blue-400 active:outline-neutral-200 duration-200 active:duration-200'>
-                        <SiLinkedin className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
-                        LinkedIn
-                    </div>
-                </Link>
-
-                <Link
-                    href='https://www.instagram.com/skylinecsc/'
-                    target='_blank'
-                >
-                    <div className='flex group w-[120px] sm:w-[134px] sm:hover:w-[140px] justify-center bg-neutral-700 dark:bg-neutral-900 bg-opacity-80 rounded-md gap-2 py-2 2 active:scale-95 hover:bg-purple-600 hover:outline-4 hover:text-white text-lg font-semibold active:outline-2 hover:outline-double outline-white border-[1px] border-neutral-800 hover:outline-purple-400 active:outline-neutral-200 duration-200 active:duration-200'>
-                        <SiInstagram className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
-                        Instagram
-                    </div>
-                </Link>
-
-                <Link
-                    href='https://github.com/Google-Developer-Groups-Skyline-College'
-                    target='_blank'
-                >
-                    <div className='flex group w-[120px] sm:w-[134px] sm:hover:w-[140px] justify-center bg-neutral-700 dark:bg-neutral-900 bg-opacity-80 rounded-md gap-1 py-2 2 active:scale-95 hover:bg-yellow-700 hover:outline-4 hover:text-white text-lg font-semibold active:outline-2 hover:outline-double outline-white border-[1px] border-neutral-800 hover:outline-yellow-700 active:outline-neutral-200 duration-200 active:duration-200'>
-                        <SiGithub className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
-                        GitHub
-                    </div>
-                </Link>
-
-                <Link href='https://linktr.ee/skylinecsc' target='_blank'>
-                    <div className='flex group w-[120px] sm:w-[134px] sm:hover:w-[140px] justify-center bg-neutral-700 dark:bg-neutral-900 bg-opacity-80 rounded-md gap-2 py-2 2 active:scale-95 hover:bg-green-900 hover:outline-4 hover:text-white text-lg font-semibold active:outline-2 hover:outline-double outline-white border-[1px] border-neutral-800 hover:outline-green-700 active:outline-neutral-200 duration-200 active:duration-200'>
-                        <SiLinktree className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
-                        LinkTree
-                    </div>
-                </Link>
-            </div>
+            <Socials/ >
 
             <div className='flex flex-row items-center justify-center gap-4 py-2 w-full'>
                 <Link href='https://skylinecollege.edu/' target='_blank'>
